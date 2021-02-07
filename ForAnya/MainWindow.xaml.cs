e4975693698f16e913a342b882ec90a93f5ab6a8
Dmitrij_Yurashevich <ivan_inav_ivan@mail.ru>
2021-02-07 20:58:23 +0300

    Версия с проверкой даты

--- ForAnya/MainWindow.xaml.cs
+++ ForAnya/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,28 @@ namespace ForAnya
     {
         public MainWindow()
         {
-            InitializeComponent();
+            
+            DateTime dr = new DateTime(2021, 2, 8, 0, 0, 0);
+            DateTime nowTime = DateTime.Now;
+            TimeSpan span = dr.Subtract(nowTime);
+
+            if(span.TotalMilliseconds > 0)
+            {
+                MessageBox.Show("до праздника еще" + "\n" +
+                "(seconds): " + span.Seconds + "\n" +
+                "(minutes): " + span.Minutes + "\n" +
+                "(hours): " + span.Hours + "\n" +
+                "ЖДИИИ))");
+                this.Close();
+            } else if(span.TotalDays < 0)
+            {
+                MessageBox.Show("День Рождение уже прошло(");
+            }
+            else
+            {
+                InitializeComponent();
+            }
+            
         }
 
         private void up(object sender, KeyEventArgs e)
@@ -35,17 +57,77 @@ namespace ForAnya
             }
             else
             {
-                
+
                 next = (TextBox)q1.FindName("block_" + (index + 1).ToString());
-                
+
             }
             if (next != null)
             {
                 next.Focus();
             }
 
-
-
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool result = checkAnswers();
+            if (result)
+            {
+                checck.Visibility = Visibility.Hidden;
+                next.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                next.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private Boolean checkAnswers()
+        {
+
+            Dictionary<string, ArrayList> answers = new Dictionary<string, ArrayList> {
+                {"копенгаген", new ArrayList{1,2,3,4,5,6,7,8,9,10} },
+                {"воронеж", new ArrayList{12,13,14,15,16,17,18} },
+                {"рязань", new ArrayList{19,20,21,22,23,24} },
+                {"мадрид", new ArrayList{25,26,27,28,29,30} },
+                {"рим", new ArrayList{31,32,33} },
+                {"таллин", new ArrayList{34,35,36,37,38,39} },
+                {"варшава", new ArrayList{40,41,42,43,44,45,46} },
+                {"тбилиси", new ArrayList{47,48,49,50,51,52,53} },
+                {"софия", new ArrayList{54,55,56,57,58} },
+                {"ньюйорк", new ArrayList{59,60,61,62,63,64,65} }
+
+            };
+
+            int count = 0;
+            foreach (KeyValuePair<string, ArrayList> entry in answers)
+            {
+                // do something with entry.Value or entry.Key
+                string word = "";
+                count = count + 1;
+                foreach(int index in entry.Value)
+                {
+                    TextBox currentBox = (TextBox)q1.FindName("block_" + (index).ToString());
+                    word += currentBox.Text;
+                }
+                if(word.ToLower() != entry.Key.ToLower())
+                {
+                    MessageBox.Show("ошибка в " + count + " cлове");
+                    return false;
+                    
+                }
+                
+            }
+            return true;
+            
+        }
+
+        private void next_Click(object sender, RoutedEventArgs e)
+        {
+            happy h = new happy(this);
+            h.Show();
+        }
+
+        
     }
 }
---
+++ ForAnya/happy.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ForAnya
+{
+    /// <summary>
+    /// Логика взаимодействия для happy.xaml
+    /// </summary>
+    public partial class happy : Window
+    {
+        public happy(Window a)
+        {
+            InitializeComponent();
+            a.Close();
+            textLabel.Content = @"Если ты читаешь, значит ты прошла кроссворд)))
+            Надеюсь, у тебя настроение огого))) Хочу пожелать тебе здоровья вагон и маленькую тележку))
+            Чтобы ты радовалась жизни, ценила как можно больше моментов)
+            Мы с тобой мало знакомы, но по тому времени, что мы общаемся, ты для меня стала замечательным человеком.
+            Я безумно рад тому, что знаю такого человечка как ты.
+            Что жизнь вот так повеливает и сводит двух далеко далеко живущих людей)
+            Я верю в то, что мы с тобой когда-то встретимся.
+            В голове, конечно, все красиво))
+            Поздравляю тебя с днем рождения) Это классный день, когда можно абсолютно все)
+            Мне очень жаль, что я не смогу тебя поздравить лично
+            Ты классная, ты всегда можешь мне написать, позвонить, я бесконечно рад, когда тебя слушаю.
+            У тебя просто сумасшедший смех))))) Ты смеешься как ребенок, и это превосходно)
+                Спасибо тебе за то что есть такая девочка как ты. ПОЗДРАВЛЛЯЮ!!!!
+                    Жми <<Далее>>            
+";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://youtu.be/qHtrjuw0hqY?t=62");
+        }
+    }
+}
